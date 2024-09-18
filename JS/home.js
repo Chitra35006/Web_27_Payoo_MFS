7131ea159ef4e1f2a1a8bc76d0d60e75b41f4ba9
Chitra35006 <ch18.cse@gmail.com>
2024-09-18 22:20:58 +0600

    adding cashOUT functionalities

--- JS/home.js
+++ JS/home.js
@@ -37,3 +37,36 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
         alert("Wrong Pin")
     }
 })
+
+//Cash Out Section
+//step:::1 add an event handler to the add money button inside the form
+document.getElementById('btn-cash-out').addEventListener('click',function(event){
+    //prevent page reload after from submit
+    event.preventDefault();
+    //step::: 2 get money to be added to the account
+    const cashOutValue1 = document.getElementById('input-cash-out');
+    const cashOutPinNumber1 = document.getElementById('cash-out-input-pin-number');
+    const cashOutValue = cashOutValue1.value;
+    const cashOutPinNumber = cashOutPinNumber1.value;
+      //step:::3 verify pin number
+    //wrong way to validate pin number
+    if(cashOutPinNumber === '1234'){
+
+          //step:::4 get the current balance
+        const currentB = document.getElementById('current-balance');
+        let currentBalance = Number(currentB.innerText);
+        let cashOutMoneyValue = Number(cashOutValue);
+
+         //step:::5 Subtract CashOutMoney  from current Balance
+        currentBalance -= cashOutMoneyValue;
+        
+        //step:::6 update the balance in the UI/Dom
+        currentB.innerText = currentBalance;
+    }
+    else{
+        alert('Can Not Cash OUT!!!!!')
+    }
+    //step:::7 empty the value
+    cashOutValue1.value = "";
+    cashOutPinNumber1.value ="";
+})
